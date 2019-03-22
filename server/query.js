1df0a4f1ef54c01dcc522bd9d36ab765d61dac36
崔海鹏 <cuihaipeng@cuihaipengdeMacBook-Pro.local>
2019-03-22 13:32:36 +0100

    basically complished

--- server/query.js
+++ server/query.js
@@ -1,13 +1,29 @@
 const { GraphQLObjectType,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLList
 } = require('graphql');
 const _ = require('lodash');
 
 const {movieType} = require('./types.js');
-const {directorType} = require('./types.js');
+// const {moviesType} = require('./types.js');
+const {populateType} = require('./types.js');
 let {movies} = require('./data.js');
-let {directors} = require('./data.js');
+let {awesome} = require('./data.js');
+
+function randomNum(minNum,maxNum){ 
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1,10); 
+        break; 
+        case 2: 
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+        break; 
+            default: 
+                return 0; 
+            break; 
+    } 
+} 
 
 
 //Define the Query
@@ -16,31 +32,61 @@ const queryType = new GraphQLObjectType({
     fields: {
         hello: {
             type: GraphQLString,
-
             resolve: function () {
                 return "Hello World";
             }
         },
-
+        populate: {
+            type: populateType,
+            resolve: function () {
+                
+                return {total:movies.length,status:200};
+            }
+        },
+        movies: {
+            type: movieType,
+            resolve: function (source, args) {
+                return awesome[randomNum(0,2)];
+            }
+        },
         movie: {
             type: movieType,
             args: {
-                id: { type: GraphQLInt }
+                id: { type: GraphQLString }
             },
             resolve: function (source, args) {
                 return _.find(movies, { id: args.id });
             }
         },
-
-        director: {
-            type: directorType,
+        search: {
+            // type: [movieType],
+            type: new GraphQLList(movieType),
             args: {
-                id: { type: GraphQLInt }
+                limit: { type: GraphQLInt },
+                metascore:{ type: GraphQLInt }
             },
             resolve: function (source, args) {
-                return _.find(directors, { id: args.id });
+                var limit0=5;
+                var metascore0=0;
+                if (args.limit)limit0=args.limit;
+                if (args.metascore)metascore0=args.metascore;     
+                // console.log(limit0);
+                var movie1 =new Array();
+                var i = 0;
+                for (m of movies){
+                    if (m.metascore>=metascore0 && i<5){
+                        movie1.push(m);
+                        console.log("________");
+                        console.log(m.metascore);
+                        console.log(metascore0);
+                        console.log(i);
+                        i=i+1;
+                    }
+                }
+                return movie1;
             }
-        }
+        },
+        
     }
 });
 
--- server/types.js
+++ server/types.js
@@ -6,35 +6,50 @@ const {
     GraphQLList
 } = require('graphql');
 
+// Define Populate Type
+populateType = new GraphQLObjectType({
+    name: 'Populate',
+    fields: {
+        total: { type: GraphQLInt },
+        status: { type: GraphQLInt }
+    }
+});
+
 // Define Movie Type
 movieType = new GraphQLObjectType({
     name: 'Movie',
     fields: {
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
+        link: { type: GraphQLString },
+        id: { type: GraphQLString },
+        metascore: { type: GraphQLInt },
+        poster: { type: GraphQLString },
+        rating: { type: GraphQLInt },
+        synopsis: { type: GraphQLString },
+        title: { type: GraphQLString },
+        votes: { type: GraphQLInt },
         year: { type: GraphQLInt },
-        directorId: { type: GraphQLID }
-
-    }
-});
-//Define Director Type
-directorType = new GraphQLObjectType({
-    name: 'Director',
-    fields: {
-        id: { type: GraphQLID },
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt },
-        movies: {
-            type: new GraphQLList(movieType),
-            resolve(source, args) {
-                return _.filter(movies, { directorId: source.id });
-            }
-
-        }
-
     }
 });
 
+// //Define Director Type
+// moviesType = new GraphQLObjectType({
+//     name: 'Movies',
+//     fields: {
+//         // id: { type: GraphQLID },
+//         // name: { type: GraphQLString },
+//         // age: { type: GraphQLInt },
+//         movies: {
+//             type: new GraphQLList(movieType),
+//             resolve(source, args) {
+//                 return _.filter(movies[1]);
+//             }
+
+//         }
+
+//     }
+// });
+
+exports.populateType = populateType;
 exports.movieType = movieType;
-exports.directorType = directorType;
+// exports.moviesType = moviesType;
 
